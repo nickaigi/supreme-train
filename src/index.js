4191e325226ee144b2dbf0b2124547238547dd4e
Nickson Kaigi <nickaigi@gmail.com>
2021-10-29 21:48:17 +0300

    composing components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Lake({name}){
+    return (
+        <h1>{name}</h1>
+    )
+}
+
+function App(){
+    return (
+        <div>
+            <Lake name='Lake Nakuru' />
+            <Lake name='Lake Naivasha' />
+            <Lake name='Lake Turkana' />
+        </div>
+    )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
